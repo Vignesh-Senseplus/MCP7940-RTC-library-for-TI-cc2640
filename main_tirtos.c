32781dc976cba9901403a162fba3c3783efa8bb1
Vignesh-Senseplus <66588541+Vignesh-Senseplus@users.noreply.github.com>
2020-06-30 11:14:32 +0530

    Update main_tirtos.c

--- main_tirtos.c
+++ main_tirtos.c
@@ -12,11 +12,7 @@
 #include <ti/sysbios/knl/Clock.h>
 #include <ti/sysbios/knl/Task.h>
 
-/* TI-RTOS Header files */
-#include <ti/drivers/PIN.h>
-#include <ti/drivers/I2C.h>
-#include <ti/drivers/UART.h>
-#include <ti/display/Display.h>
+
 
 /* Example/Board Header files */
 #include "Board.h"
@@ -26,37 +22,8 @@
 
 #define TASKSTACKSIZE 1024
 
-
-
-
-uint8_t      timestamp[7];
-
-//#define BoostEn IOID_9
-/* Global memory storage for a PIN_Config table */
-
-
-/*
- * Application LED pin configuration table:
- * - All LEDs board LEDs are off.
- */
-Display_Handle dispHandle = NULL;
-
 Task_Struct task0Struct;
 Char task0Stack[TASKSTACKSIZE];
-static PIN_State ledPinState;
-static PIN_Handle ledPinHandle;
-PIN_Config ledPinTable[] = {
-                            Board_PIN_LED0 | PIN_GPIO_OUTPUT_EN | PIN_GPIO_LOW | PIN_PUSHPULL | PIN_DRVSTR_MAX,
-                            Board_PIN_LED1 | PIN_GPIO_OUTPUT_EN | PIN_GPIO_LOW | PIN_PUSHPULL | PIN_DRVSTR_MAX,
-                            SE | PIN_GPIO_OUTPUT_EN | PIN_GPIO_HIGH | PIN_PUSHPULL | PIN_DRVSTR_MAX,
-                            PIN_TERMINATE
-};
-UART_Handle uart;
-UART_Params uartParams;
-I2C_Handle i2c;
-I2C_Params i2cParams;
-I2C_Transaction i2cTransaction;
-
 
 uint8_t convertFromBcd(uint8_t byteBCD);
 uint8_t convertToBcd(uint8_t byteDecimal);
@@ -65,7 +32,7 @@ void init_MCP7940();
 
 Void taskFxn(UArg arg0, UArg arg1)
 {
-    uint8_t data[7]={0,07,14,6,27,03,15};
+    uint8_t data[7]={0,07,14,6,27,03,15};//datas sent to rtc..0sec,07min,14hrs,6dy of the week,27day,03month,15year.
 
 
 
@@ -119,48 +86,18 @@ int main(void)
 {
 
     Task_Params taskParams;
-    uint32_t st;
-
-    /* Call board init functions */
-    // Board_initGeneral();
-    //Board_init();
-
     PIN_init(BoardGpioInitTable);
-   // I2C_init();
-    UART_init();
-
-
-
-    /* Create a UART with data processing off. */
-    UART_Params_init(&uartParams);
-    uartParams.writeDataMode = UART_DATA_BINARY;
-    uartParams.readDataMode = UART_DATA_BINARY;
-    uartParams.readReturnMode = UART_RETURN_FULL;
-    uartParams.readEcho = UART_ECHO_OFF;
-    uartParams.baudRate = 115200;
-    uart = UART_open(Board_UART0, &uartParams);
-
-    if (uart == NULL) {
-        //System_abort("Error opening the UART");
-    }
-
+   
     /* Construct tmp007 Task thread */
     Task_Params_init(&taskParams);
     taskParams.stackSize = TASKSTACKSIZE;
     taskParams.stack = &task0Stack;
     Task_construct(&task0Struct, (Task_FuncPtr)taskFxn, &taskParams, NULL);
 
-    /* Open LED pins */
-    ledPinHandle = PIN_open(&ledPinState, ledPinTable);
-    if(!ledPinHandle) {
-        System_abort("Error initializing board LED pins\n");
-    }
+   
 
 
-
-
-    // PIN_setOutputValue(ledPinHandle, Board_LED2, 0);
-    //PIN_setPortOutputValue(ledPinHandle, 1 );
+  
     /* Start BIOS */
     BIOS_start();
 
